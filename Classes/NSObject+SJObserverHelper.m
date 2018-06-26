c57c3de758074839e3b214d68525f53c1afb8c9b
SanJiang <changsanjiang@gmail.com>
2018-06-26 13:47:42 +0800

    增加了非空判断

--- Classes/NSObject+SJObserverHelper.m
+++ Classes/NSObject+SJObserverHelper.m
@@ -9,8 +9,9 @@
 #import "NSObject+SJObserverHelper.h"
 #import <objc/message.h>
 
+NS_ASSUME_NONNULL_BEGIN
 @interface SJObserverHelper : NSObject
-@property (nonatomic, readonly) const char *key;
+@property (nonatomic, readonly) const char *key; // lazy load
 @property (nonatomic, unsafe_unretained) id target;
 @property (nonatomic, unsafe_unretained) id observer;
 @property (nonatomic, strong) NSString *keyPath;
@@ -18,29 +19,44 @@
 @end
 
 @implementation SJObserverHelper {
-    char * _key;
+    char *_key;
 }
 - (instancetype)init {
     self = [super init];
     if ( !self ) return nil;
-    NSString *keyStr = [NSString stringWithFormat:@"adfsf:%lu", (unsigned long)[self hash]];
-    _key = malloc(keyStr.length * sizeof(char) + 1);
-    strcpy(_key, keyStr.UTF8String);
+    _key = NULL;
     return self;
 }
+- (const char *)key {
+    if ( _key ) return _key;
+    NSString *keyStr = [NSString stringWithFormat:@"sanjiang:%lu", (unsigned long)[self hash]];
+    _key = malloc(keyStr.length * sizeof(char) + 1);
+    strcpy(_key, keyStr.UTF8String);
+    return _key;
+}
 - (void)dealloc {
-    free(_key);
+    if ( _key ) free(_key);
     if ( _factor ) {
         [_target removeObserver:_observer forKeyPath:_keyPath];
     }
 }
 @end
 
+
+
 @implementation NSObject (ObserverHelper)
 
 - (void)sj_addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath {
+    [self sj_addObserver:observer forKeyPath:keyPath context:NULL];
+}
+
+- (void)sj_addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath context:(nullable void *)context {
+    NSParameterAssert(observer);
+    NSParameterAssert(keyPath);
     
-    [self addObserver:observer forKeyPath:keyPath options:NSKeyValueObservingOptionNew context:nil];
+    if ( !observer || !keyPath ) return;
+    
+    [self addObserver:observer forKeyPath:keyPath options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:nil];
     
     SJObserverHelper *helper = [SJObserverHelper new];
     SJObserverHelper *sub = [SJObserverHelper new];
@@ -48,12 +64,13 @@
     sub.target = helper.target = self;
     sub.observer = helper.observer = observer;
     sub.keyPath = helper.keyPath = keyPath;
+    
     helper.factor = sub;
     sub.factor = helper;
     
     objc_setAssociatedObject(self, helper.key, helper, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    objc_setAssociatedObject(observer, sub.key, sub, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(observer, helper.key, sub, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
-
+NS_ASSUME_NONNULL_END
